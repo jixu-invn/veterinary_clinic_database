cd6f79f51167384062f1e04f049c45bf25ec8a3c
Marc Damie <marc.damie@etu.utc.fr>
2019-05-20 08:47:42 +0200

    INSERT des clients

---
+++ RO imbrication/INSERT.sql
@@ -0,0 +1,22 @@
+INSERT INTO Clients VALUES 
+                (1,
+                'Jugnot',
+                'Gerard',
+                TO_DATE('1955-12-04','YYYY-MM-DD'),
+                '255 avenue foch, Paris',
+                '0745869621');
+                
+INSERT INTO Clients VALUES 
+                (2,
+                'Belmondo',
+                'Jean-Paul',
+                TO_DATE('1964-09-10','YYYY-MM-DD'),
+                '1024 boulevard de la vilette, Niors',
+                '0515487959');
+INSERT INTO Clients VALUES 
+                (3,
+                'Cotillard',
+                'Marion',
+                TO_DATE('1985-04-12','YYYY-MM-DD'),
+                '41 rue de neuilly, Nanterre',
+                '0256368596');
