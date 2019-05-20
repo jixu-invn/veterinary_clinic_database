30ee73670be027b1bcd2f64a33bd01474583bcaf
Marc Damie <marc.damie@etu.utc.fr>
2019-05-20 09:04:29 +0200

    Update INSERT.sql

--- RO imbrication/INSERT.sql
+++ RO imbrication/INSERT.sql
@@ -1,5 +1,5 @@
 INSERT INTO Clients VALUES 
-                (1,
+                (9001,
                 'Jugnot',
                 'Gerard',
                 TO_DATE('1955-12-04','YYYY-MM-DD'),
@@ -7,23 +7,23 @@ INSERT INTO Clients VALUES
                 '0745869621');
                 
 INSERT INTO Clients VALUES 
-                (2,
+                (9002,
                 'Belmondo',
                 'Jean-Paul',
                 TO_DATE('1964-09-10','YYYY-MM-DD'),
                 '1024 boulevard de la vilette, Niors',
                 '0515487959');
 INSERT INTO Clients VALUES 
-                (3,
+                (9003,
                 'Cotillard',
                 'Marion',
                 TO_DATE('1985-04-12','YYYY-MM-DD'),
                 '41 rue de neuilly, Nanterre',
                 '0256368596');
 
-INSERT INTO Animaux VALUES (1, 'Griffon', 25.3, 1.058, 2012, 1, TypEspece('chien', 'toutou'));
-INSERT INTO Animaux VALUES (2, 'Manu', 208.7, 2.584, 2013, 2, TypEspece('crocodile','reptile'));
-INSERT INTO Animaux VALUES (3, 'Myrtille', 2.4, 0.268, 2014, 3, TypEspece('truite','poisson'));
+INSERT INTO Animaux VALUES (1, 'Griffon', 25.3, 1.058, 2012, 9001, TypEspece('chien', 'toutou'));
+INSERT INTO Animaux VALUES (2, 'Manu', 208.7, 2.584, 2013, 9002, TypEspece('crocodile','reptile'));
+INSERT INTO Animaux VALUES (3, 'Myrtille', 2.4, 0.268, 2014, 9003, TypEspece('truite','poisson'));
 
 INSERT INTO Traitements VALUES 
                 (2001,
