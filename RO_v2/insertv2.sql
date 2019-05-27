09023fa070127720267f259cf65f8f9b384b2cc2
Alix Candusso <alix.candusso@etu.utc.fr>
2019-05-27 20:28:05 +0200

    Ajout de l'insertion d'un veto et d'un traitement. (Normalement ça compile?)

--- RO_v2/insertv2.sql
+++ RO_v2/insertv2.sql
@@ -13,3 +13,31 @@ where Es.nom = 'chien';
 insert into medoc values('rinoxolone','desc_rinoxolone', listeEspecesAutorisees(RefEspece(ref1)));
 
 end;
+
+
+insert into veto values('prof', 'oak', TO_DATE('1111-11-11','YYYY-MM-DD'),'', '', 'mammifere');
+
+declare
+ref2 ref TypMedoc;
+ref3 ref TypVeto;
+
+begin
+
+select ref(Me) into ref2
+from Medoc Me
+where Me.molecule = 'rinoxolone';
+
+select ref(Ve) into ref3
+from Veto Ve
+where Ve.nom = 'prof' and Ve.prenom = 'oak' and Ve.naissance = TO_DATE('1111-11-11','YYYY-MM-DD');
+
+
+insert into Traitements values(
+    2001,
+    'traitement foie',
+    TO_DATE('2015-06-06','YYYY-MM-DD'),
+    TO_DATE('2015-06-10','YYYY-MM-DD'),
+    listeMedoc(
+        RefMedocs(ref2, 20)),
+    ref3);
+end;
