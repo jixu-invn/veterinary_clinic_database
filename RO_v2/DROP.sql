c78630a7fea043dd805778e3f8a0e43d6d90b50f
Marc Damie <marc.damie@etu.utc.fr>
2019-05-27 11:48:47 +0200

    Update DROP.sql

--- RO_v2/DROP.sql
+++ RO_v2/DROP.sql
@@ -1,8 +1,19 @@
-DROP TABLE Traitements;
-DROP TABLE Animaux;
 DROP TABLE Clients;
+DROP TABLE Animaux;
+DROP TABLE Medoc;
+DROP TABLE Especes;
+DROP TABLE Assistants;
+DROP TABLE Veto;
+DROP TABLE Classes_especes_animales;
 
+DROP TYPE listeAnimaux;
+DROP TYPE RefAnimaux;
+DROP TYPE TypAnimal;
+DROP TYPE ListeTraitement:
 DROP TYPE TypTraitement;
+DROP TYPE TypAssistant;
+DROP TYPE TypVeto;
+DROP TYPE Individu;
 DROP TYPE ListeMedoc;
 DROP TYPE RefMedocs;
 DROP TYPE TypMedoc;
