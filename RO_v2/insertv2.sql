798d61577e1577cb25b7b59494d198e8218532a9
Alix Candusso <alix.candusso@etu.utc.fr>
2019-05-27 20:06:32 +0200

    Add new file

---
+++ RO_v2/insertv2.sql
@@ -0,0 +1,15 @@
+insert into Classes_especes_animales values('mammifere');
+insert into Especes values('chien', 'mammifere');
+
+declare 
+ref1 ref TypEspece;
+
+begin
+
+select ref(Es) into ref1
+from Especes Es
+where Es.nom = 'chien';
+
+insert into medoc values('rinoxolone','desc_rinoxolone',listeEspecesAutorisees(ref1));
+
+end;
