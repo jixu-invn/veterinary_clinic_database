b34d758e86ed2a41904bdc533ca77b75e93b30ce
Valentin Rousseau <valentin.rousseau@etu.utc.fr>
2019-05-28 09:52:33 +0200

    rajout de données, ça compile.

--- RO_v2/insertv2.sql
+++ RO_v2/insertv2.sql
@@ -84,3 +84,70 @@ insert into Clients values(
     
 end;
 
+
+--------------------------------------------------
+
+insert into Classes_especes_animales values('poisson');
+insert into Especes values('truite','poisson');
+
+declare 
+r1 ref TypEspece;
+
+begin
+select ref(E) into r1
+from Especes E
+where E.nom = 'truite';
+
+insert into medoc values('bronchioture','nettoie les bronchies', listeEspecesAutorisees(RefEspece(r1)));
+
+end;
+
+insert into veto values('Corsair','Pascal',TO_DATE('1974-07-22','YYYY-MM-DD'),'5 rue de l''auberge, Reims','0548789631','poisson'); 
+insert into assistants values('Gaudron','Michel',TO_DATE('1978-06-27','YYYY-MM-DD'),'40 rue de la rose, Compiegne','0542788965','poisson');
+
+declare 
+r2 ref TypMedoc;
+r3 ref TypVeto;
+
+begin
+
+select ref(M) into r2
+from Medoc M
+where M.molecule = 'bronchioture';
+
+select ref(V) into r3
+from Veto V
+where V.nom = 'Corsair' and V.prenom = 'Pascal' and V.naissance = TO_DATE('1974-07-22','YYYY-MM-DD');
+
+insert into Traitements values(
+	2002,'traitement bronchie',TO_DATE('2015-06-08','YYYY-MM-DD'),TO_DATE('2015-06-18','YYYY-MM-DD'),
+	listeMedoc(RefMedocs(r2,10)),r3);
+end;
+
+declare r4 ref TypTraitement;
+
+begin
+
+select ref(T) into r4
+from Traitements T
+where T.id = 2002;
+
+insert into Animaux values(3,'Myrtille',2.4,.268,2014,'truite',ListeTraitement(RefTrait(r4)));
+
+end;
+
+declare 
+r5 ref TypAnimal;
+
+begin 
+
+select ref(A) into r5
+from Animaux A
+where A.idAnimal = 3;
+
+insert into Clients values(9003,'Cotillard','Marion',TO_DATE('1985-04-12','YYYY-MM-DD'),
+'41 rue de neuilly, Nanterre','0256368596',listeAnimaux(RefAnimaux(r5)));
+
+end;
+
+
