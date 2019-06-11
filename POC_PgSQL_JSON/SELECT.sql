1cc9734f6da699c94bfc433d4bb41680186c2d95
Valentin Rousseau <valentin.rousseau@etu.utc.fr>
2019-06-11 11:13:44 +0200

    Update SELECT.sql

--- POC_PgSQL_JSON/SELECT.sql
+++ POC_PgSQL_JSON/SELECT.sql
@@ -1,2 +1,7 @@
 SELECT DISTINCT a.id, a.nom, t->'veto'->>'nom' nom_veto,t->'veto'->>'pnom'prénom_veto
 FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t;
+
+
+SELECT DISTINCT a.id, a.nom as animal_medoc_riboflux
+FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t
+where t->'medicament'->>'molecule'='riboflux';
