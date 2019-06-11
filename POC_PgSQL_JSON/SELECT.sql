ac1edc6786613d1ac55621eaa365513942092274
Marc Damie <marc.damie@etu.utc.fr>
2019-06-11 15:47:15 +0200

    Correction du select erroné qui avait été supprimé

--- POC_PgSQL_JSON/SELECT.sql
+++ POC_PgSQL_JSON/SELECT.sql
@@ -1,3 +1,9 @@
 SELECT a.id, a.nom, t->'veto'->>'nom' nom_veto,t->'veto'->>'pnom' prénom_veto, COUNT(*) nb_traitements_prescrits
 FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t
 GROUP BY a.id, a.nom, t->'veto'->>'nom',t->'veto'->>'pnom';
+
+-- SELECT supplémentaire: chaque animal ayant eu au moins une prescriptions de riboflux et le nombre de prescriptions reçues de ce médicament
+SELECT a.id, a.nom as animal_medoc_riboflux, COUNT(*) as nb_prescriptions
+FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t, JSON_ARRAY_ELEMENTS(t->'medicaments') m 
+WHERE LOWER(m->>'molecule')='riboflux'
+GROUP BY a.id, a.nom;
