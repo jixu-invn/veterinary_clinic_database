8ebbea308dd04851a1e66980ed4999db0931dd41
Marc 'T0K_' D <marc.damie@etu.utc.fr>
2019-06-21 15:14:57 +0200

    Ajout de deux stats dont une bonus

--- appli_python/affichage_tables.py
+++ appli_python/affichage_tables.py
@@ -45,6 +45,7 @@ def printMedocPrescrits(connexion):
  
  
 def printStatMoyennesEspeces(connexion):
+    print("Statistique : Caractéristiques moyennes de chaque espèce")
     t = PrettyTable(['Espece','Poids moyen','Taille moyenne'])
     resultat = connexion.cursor()
     resultat.execute("SELECT A.espece, AVG(A.dernier_poids) PoidsMoyen,AVG(A.derniere_taille) TailleMoyenne \
@@ -56,11 +57,36 @@ def printStatMoyennesEspeces(connexion):
     print(t)
 
 def printStatGlobalesMedoc(connexion):
+    print("Statistique : Quantité totale prescrite de chaque médicament")
     t = PrettyTable(['Molecule','Quantité totale prescrite'])
     resultat = connexion.cursor()
     resultat.execute("SELECT m->>'molecule', SUM(CAST(m->>'qte' AS INTEGER)*(TO_DATE(t->>'fin','YYYY-MM-DD')-TO_DATE(t->>'debut','YYYY-MM-DD')+1)) \
                     FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t, JSON_ARRAY_ELEMENTS(t->'medicaments') m\
                     GROUP BY m->>'molecule'")
+    
+    for ligne in resultat.fetchall():
+        t.add_row(ligne)
+    print(t)
+
+def printStatQteMedocParAnimal(connexion):
+    print("Statistique : Quantité de chaque médicament prescrite à chaque animal")
+    t = PrettyTable(['ID Animal','Molecule','Quantité totale prescrite'])
+    resultat = connexion.cursor()
+    resultat.execute("SELECT a.id, m->>'molecule', SUM(CAST(m->>'qte' AS INTEGER)*(TO_DATE(t->>'fin','YYYY-MM-DD')-TO_DATE(t->>'debut','YYYY-MM-DD')+1)) \
+                    FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t, JSON_ARRAY_ELEMENTS(t->'medicaments') m\
+                    GROUP BY a.id, m->>'molecule'")
+    
+    for ligne in resultat.fetchall():
+        t.add_row(ligne)
+    print(t)
+
+def printStatBonus(connexion):
+    print("Statistique bonus: Nombre de vétérinaires consultés par chaque client")
+    t = PrettyTable(['Nom','Prenom','Naissance','Nombre de vétérinaires consultés'])
+    resultat = connexion.cursor()
+    resultat.execute("SELECT C.nom, C.prenom, C.naissance, COUNT(DISTINCT t->>'veto') AS Nb_veto_consultes \
+    FROM Animaux A JOIN Clients C ON A.proprietaire = C.id, JSON_ARRAY_ELEMENTS(a.traitements) t \
+    GROUP BY C.nom, C.prenom, C.naissance")
     
     for ligne in resultat.fetchall():
         t.add_row(ligne)
@@ -73,4 +99,4 @@ if __name__ == '__main__':
     printClients(conn)
     printAnimaux(conn)
     printMedocPrescrits(conn)
-    printStatGlobalesMedoc(conn)
+    printStatBonus(conn)
