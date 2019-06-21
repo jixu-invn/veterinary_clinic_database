122287e2a18a62fef6a60b5f2944ee9843ec26bf
Alix Candusso <alix.candusso@etu.utc.fr>
2019-06-21 14:29:40 +0200

    fonctions de creation de Medicament, Veto, Traitement

---
+++ appli_python/creationTraitementDict.py
@@ -0,0 +1,46 @@
+def creationVetoJSON() :
+    nom = str(input("Nom du vétérinaire : "))
+    prenom = str(input("Prenom du vétérinaire : "))
+    return({"nom":nom, "prenom":prenom})
+            
+def creationMedicamentJSON() :
+    molecule = str(input("Nom du médicament : "))
+    while True :
+        try :
+            conditionnement = int(input("Conditionnement du médicament : "))
+            break
+        except :
+            print("Le conditionnement doit être un entier!")
+    return({"molecule" : molecule, "conditionnement" : conditionnement})
+
+
+
+def creationTraitementJSON() :
+    dateDebut = input("Date de début de traitement (YYYY-MM-DD) : ")
+    dateFin = input("Date de fin de traitement (YYYY-MM-DD) : ")
+    veto = creationVetoJSON()
+    
+    while True :
+        try : 
+            nbMedicaments = int(input("Combien de médicaments sont inclus dans le traitement? : "))
+            break
+        except :
+            print("Entrez un nombre entier")
+    listeMedocs = []
+    for i in range(0, nbMedicaments, 1) :
+        medicament = creationMedicamentJSON()
+        listeMedocs.append(medicament)
+    
+    traitement = {"debut" : dateDebut,
+                  "fin" : dateFin,
+                  "veto" : veto,
+                  "medicaments" : listeMedocs}
+    
+    return(traitement)
+                  
+    
+    
+if __name__ == "__main__" :
+    print(creationTraitementJSON())
+    #print(creationVetoJSON())
+    #print(creationMedicamentJSON())
