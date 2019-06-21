1dbffd56d44f224720bfad4ea43d358e2e1cb226
Marc 'T0K_' D <marc.damie@etu.utc.fr>
2019-06-21 15:02:37 +0200

    Creation stats 2/3 done

--- appli_python/affichage_tables.py
+++ appli_python/affichage_tables.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import psycopg2 as psy
 from prettytable import PrettyTable
+from datetime import date
 
 def printClients(connexion):
     t = PrettyTable(['id','Nom','Prenom','Naissance','Adresse', 'Tel'])
@@ -20,7 +21,8 @@ def printEspeces(connexion):
     for ligne in resultat.fetchall():
         t.add_row(ligne)
     print(t)
-    
+ 
+ 
 def printAnimaux(connexion):
     t = PrettyTable(['ID Animal','Nom','Poids', 'Taille', 'Année de naissance','Espèce', 'ID Propriétaire'])
     resultat = connexion.cursor()
@@ -29,7 +31,8 @@ def printAnimaux(connexion):
     for ligne in resultat.fetchall():
         t.add_row(ligne)
     print(t)
-    
+ 
+ 
 def printMedocPrescrits(connexion):
     t = PrettyTable(['ID Animal','ID proprietaire','Medicament prescrit'])
     resultat = connexion.cursor()
@@ -39,7 +42,30 @@ def printMedocPrescrits(connexion):
     for ligne in resultat.fetchall():
         t.add_row(ligne)
     print(t)
+ 
+ 
+def printStatMoyennesEspeces(connexion):
+    t = PrettyTable(['Espece','Poids moyen','Taille moyenne'])
+    resultat = connexion.cursor()
+    resultat.execute("SELECT A.espece, AVG(A.dernier_poids) PoidsMoyen,AVG(A.derniere_taille) TailleMoyenne \
+                        FROM Animaux A \
+                        GROUP BY A.espece")
     
+    for ligne in resultat.fetchall():
+        t.add_row(ligne)
+    print(t)
+
+def printStatGlobalesMedoc(connexion):
+    t = PrettyTable(['Molecule','Quantité totale prescrite'])
+    resultat = connexion.cursor()
+    resultat.execute("SELECT m->>'molecule', SUM(CAST(m->>'qte' AS INTEGER)*(TO_DATE(t->>'fin','YYYY-MM-DD')-TO_DATE(t->>'debut','YYYY-MM-DD')+1)) \
+                    FROM animaux a, JSON_ARRAY_ELEMENTS(a.traitements) t, JSON_ARRAY_ELEMENTS(t->'medicaments') m\
+                    GROUP BY m->>'molecule'")
+    
+    for ligne in resultat.fetchall():
+        t.add_row(ligne)
+    print(t)
+
 
 if __name__ == '__main__':
     conn = psy.connect("dbname = 'dbbdd0p050' user='bdd0p050' host='tuxa.sme.utc' password='oN37PaLy'")
@@ -47,3 +73,4 @@ if __name__ == '__main__':
     printClients(conn)
     printAnimaux(conn)
     printMedocPrescrits(conn)
+    printStatGlobalesMedoc(conn)
